6d7d3d30dc0865b3938b8c0eb0ce3dd0f9b65549
knika-starkey <knika787@gmail.com>
2022-10-07 21:53:46 +0300

    add random

--- script.js
+++ script.js
@@ -1,41 +1,56 @@
 "use strict";
-let location1 = 3; // Розташування 1 клітини корабля
-let location2 = 4; // Розташування 2 клітини корабля
-let location3 = 5; // Розташування 3 клітини корабля
+let randomShipLength = Math.floor(Math.random() * (4 - 1) + 1);
+let randomLength = Math.floor(Math.random() * (15 - 4) + 4);
+let randomLoc = Math.floor(Math.random() * (randomLength - randomShipLength));
+let shiploc = [];
+console.log(
+  `${randomShipLength} довжина корабля, ${randomLength} довжина поля, ${randomLoc} локація`
+);
+for (let i = 0; i < randomShipLength; i++) {
+  shiploc.push(randomLoc++);
+}
+console.log(shiploc);
 let guess; // координата осередку
 let hits = 0; // влучення
 let guesses = 0; //Спроби
+let ishit = false;
 let isSunk = false;
 let shots = [];
 label: while (!isSunk) {
-  guess = prompt("Ваш постріл! (введіть число від 0 до 6):");
+  guess = prompt(`Ваш постріл! (введіть число від 0 до ${randomLength - 1}):`);
   if (isNaN(guess)) {
     break;
   }
 
-  if (guess < 0 || guess > 6) {
+  if (guess < 0 || guess > randomLength - 1) {
     alert("Введіть правильний номер!");
     continue;
   }
   for (let i = 0; i < shots.length; i++) {
     if (shots[i] == guess) {
       alert("В цю клітинку вже був постріл!");
+
       //continue;
       break label;
     }
   }
   shots.push(guess);
   guesses++;
-  if (guess == location1 || guess == location2 || guess == location3) {
-    alert("Влучив!");
-    hits++;
-    if (hits == 3) {
-      isSunk = true;
-      alert("Ви втопили корабель!");
+  for (let i = 0; i < shiploc.length; i++) {
+    if (guess == shiploc[i]) {
+      alert("Влучив!");
+      hits++;
+      ishit = true;
+      if (hits == shiploc.length) {
+        isSunk = true;
+        alert("Ви втопили корабель!");
+      }
     }
-  } else {
+  }
+  if (!ishit) {
     alert("Промах!");
   }
+  ishit = false;
 }
 console.log(shots);
 alert(
